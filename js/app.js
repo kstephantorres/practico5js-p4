b6dc397deb859d541ba9f51e47788082020a6a9c
K Stephan Torres <kevinstephantorres@gmail.com>
2023-11-28 01:10:40 -0300

    Se agrego la fecha a los divs

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const dosDigitos=(numero)=> numero<10? '0'+numero:numero
+
+const meses = {
+    0: 'Enero',
+    1: 'Febrero',
+    2: 'Marzo',
+    3: 'Abril',
+    4: 'Mayo',
+    5: 'Junio',
+    6: 'Julio',
+    7: 'Agosto',
+    8: 'Septiembre',
+    9: 'Octubre',
+    10: 'Noviembre',
+    11: 'Diciembre'
+}
+const dias = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado',
+}
+
+setInterval(()=>{
+const fecha = new Date()
+const divs = document.getElementsByClassName('transparente')
+const hora = `${dosDigitos(fecha.getHours())}:${dosDigitos(fecha.getMinutes())}:${dosDigitos(fecha.getSeconds())}`
+const diaSemana = fecha.getDay();
+const dia = fecha.getDate()
+const mes = fecha.getMonth()
+const anio = fecha.getFullYear()
+
+divs[0].innerHTML = `${dias[diaSemana]} ${dosDigitos(dia)} de ${meses[mes]} del ${anio}`
+divs[1].innerHTML = hora
+
+},1000)
